1c77e0834ee04c70f31bcea3de5266f5f9967a6d
Guyititian <128245262+Guyititian@users.noreply.github.com>
2026-04-18 19:18:48 -0400

    Update assets.js

--- assets.js
+++ assets.js
@@ -21,7 +21,7 @@ async function fetchCategory(category) {
   if (!Array.isArray(data)) return [];
 
   return data
-    .filter(f => f.name.match(/\.(jpeg|jpg|png|webp)$/i))
+    .filter(f => f.name.match(/\.(jpeg|jpg|png|webp|gif)$/i))
     .map(f => ({
       name: f.name.replace(/\.[^/.]+$/, ""),
       url: `${baseURL}collections/${category}/${f.name}`,
